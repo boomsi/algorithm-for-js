e8ee123309b2fbf2e6b6f885b43f40372dc4e34e
boomsi <libo19431@gmail.com>
2021-07-23 10:18:24 +0800

    feat:add quick again

--- 11_sort/quick.js
+++ 11_sort/quick.js
@@ -31,37 +31,38 @@ console.log(a);
 module.exports = sort_quick;
 
 
+// 再来亿遍
+// function sort_quick (list = [], start = 0, end = list.length - 1) {
+//     if (start >= end) {
+//         return;
+//     }
 
-// 快排(循环)
-function quickSortIterative(arr) {
-  // 用push()和pop()函数创建一个将作为栈使用的数组
-  stack = [];
+//     const pivot = generatePivot(list, start, end)
 
-  // 将整个初始数组做为“未排序的子数组”
-  stack.push(0);
-  stack.push(arr.length - 1);
+//     sort_quick(list, start, pivot - 1)
+//     sort_quick(list, pivot + 1, end)
+    
+// }
 
-  // 没有显式的peek()函数
-  // 只要存在未排序的子数组，就重复循环
-  while (stack[stack.length - 1] >= 0) {
-    // 提取顶部未排序的子数组
-    end = stack.pop();
-    start = stack.pop();
+// function generatePivot(list, start, end) {
+//     const pivotValue = list[end]
+//     let pivotIndex = start
 
-    pivotIndex = partition(arr, start, end);
+//     for(let i = start; i < end; i ++) {
+//         if (list[i] < pivotValue) {
+//             [list[i], list[pivotIndex]] = [list[pivotIndex], list[i]]
+//             pivotIndex ++
+//         }
+//     }
 
-    // 如果基准的左侧有未排序的元素，
-    // 则将该子数组添加到栈中，以便稍后对其进行排序
-    if (pivotIndex - 1 > start) {
-      stack.push(start);
-      stack.push(pivotIndex - 1);
-    }
+//     [list[pivotIndex], list[end]] = [list[end], list[pivotIndex]]
 
-    // 如果基准的右侧有未排序的元素，
-    // 则将该子数组添加到栈中，以便稍后对其进行排序
-    if (pivotIndex + 1 < end) {
-      stack.push(pivotIndex + 1);
-      stack.push(end);
-    }
-  }
-}
+//     return pivotIndex
+// }
+
+
+// a = [7, -2, 4, 1, 6, 5, 0, -4, 0, 2];
+// sort_quick(a);
+// console.log(a);
+
+
